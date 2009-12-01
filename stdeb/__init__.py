d26a78d3e0695e0bf492910c530beb54b30cdbbc
Andrew Straw <strawman@astraw.com>
2009-12-01 09:29:56 -0800

    bump version number for development

--- stdeb/__init__.py
+++ stdeb/__init__.py
@@ -1,7 +1,7 @@
 # setuptools is required for distutils.commands plugin we use
 import logging
 import setuptools
-__version__ = '0.4.2'
+__version__ = '0.4.2.git'
 
 log = logging.getLogger('stdeb')
 log.setLevel(logging.INFO)
