ca065abbd81a1d9c882b4581f9532b0475a2d8be
Andrew Straw <strawman@astraw.com>
2009-12-30 18:54:41 -0800

    replace long_description text with entire README.rst file

--- setup.py
+++ setup.py
@@ -7,10 +7,7 @@ setup(name='stdeb',
       author='Andrew Straw',
       author_email='strawman@astraw.com',
       description='Python to Debian source package conversion utility',
-      long_description=\
-      """stdeb extends distutils to make Debian source packages from Python
-packages. It attempts to provide automatic defaults, but many aspects
-of the resulting package can be customized via a configuration file.""",
+      long_description=open('README.rst').read(),
       license='MIT',
       url='http://github.com/astraw/stdeb',
       packages=['stdeb','stdeb.command'],
