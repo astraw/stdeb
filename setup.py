d729408605d31ebf44d148635016a856b366f740
Andrew Straw <strawman@astraw.com>
2010-02-05 09:03:48 -0800

    bump version for development

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(name='stdeb',
       # Keep version in sync with stdeb/__init__.py, Install section
       # of README.rst, and USER_AGENT in scripts/pypi-install.
-      version='0.5.1',
+      version='0.5.1+git',
       author='Andrew Straw',
       author_email='strawman@astraw.com',
       description='Python to Debian source package conversion utility',
