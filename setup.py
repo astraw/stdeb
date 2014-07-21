39bd648c197a8a11f1e38ee35dbef3da426a9a81
Andrew Straw <strawman@astraw.com>
2014-07-21 12:19:00 +0200

    remove outdated check for no-longer-used functionality

--- setup.py
+++ setup.py
@@ -1,18 +1,6 @@
 from distutils.core import setup
 import os
 
-if int(os.environ.get('STDEB_CHECK_DEBIAN_PYTHON','1')):
-
-    # http://travis-ci.org/ seems to run with a Python not distributed
-    # by Ubuntu/Debian and thus misses critical functionality to play
-    # nicely with the Debian packaging utilities.
-
-    import distutils.command.install as imod
-    opts=[x for x in imod.install.user_options if x[0]=='install-layout=']
-    if len(opts)==0:
-        raise RuntimeError('Your distutils install command does not support '
-                           'the --install-layout option. stdeb will fail.')
-
 setup(name='stdeb',
       # Keep version in sync with stdeb/__init__.py, Install section
       # of README.rst, and USER_AGENT in scripts/pypi-install.
