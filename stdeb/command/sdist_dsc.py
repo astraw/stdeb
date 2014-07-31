87e7c0ebda58dd2c65e98eeab5b2141fdeca95b5
Andrew Straw <strawman@astraw.com>
2014-07-31 12:28:09 +0200

    remove unneeded imports

--- stdeb/command/sdist_dsc.py
+++ stdeb/command/sdist_dsc.py
@@ -2,7 +2,7 @@ import sys, os, shutil, tempfile
 
 from stdeb import log
 from stdeb.util import expand_sdist_file, recursive_hardlink
-from stdeb.util import DebianInfo, build_dsc, stdeb_cmdline_opts, \
+from stdeb.util import build_dsc, stdeb_cmdline_opts, \
      stdeb_cmd_bool_opts, stdeb_cfg_options
 from stdeb.util import repack_tarball_with_debianized_dirname
 from stdeb.command.common import common_debian_package_command
