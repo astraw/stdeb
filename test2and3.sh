6e7021f757ffe0495362e2a40de9512ca387495b
Andrew Straw <strawman@astraw.com>
2014-08-16 09:06:26 +0200

    testing: verify py2 and py3 installs before testing

--- test2and3.sh
+++ test2and3.sh
@@ -1,18 +1,22 @@
 #!/bin/bash
 set -e
 
+export DO_PY2=true
+export DO_PY3=true
+
 # setup paths
 
 if [ "${PY2EXE}" == "" ]; then
-  PY2EXE=`which python2`;
+  PY2EXE=`which python2` || export DO_PY2=false
 fi
 
 if [ "${PY3EXE}" == "" ]; then
-  PY3EXE=`which python3`;
+  PY3EXE=`which python3` || export DO_PY3=false
 fi
 
-echo "using Python 2 at ${PY2EXE}"
-echo "using Python 3 at ${PY3EXE}"
+# check that stdeb is actually installed
+${PY2EXE} -c "import stdeb" || export DO_PY2=false
+${PY3EXE} -c "import stdeb" || export DO_PY3=false
 
 ## Tell Python that we do not have e.g. UTF-8 file encodings and thus
 ## force everything to be very explicit.
@@ -20,11 +24,20 @@ export LC_ALL="en_US"
 
 ## Test very basic py2 and py3 packages ------
 
-cd test_data/py2_only_pkg
-${PY2EXE} setup.py --command-packages stdeb.command sdist_dsc --with-python2=true --with-python3=false bdist_deb
-cd ../..
+if [ "$DO_PY2" = true ]; then
+    echo "using Python 2 at ${PY2EXE}"
+    cd test_data/py2_only_pkg
+    ${PY2EXE} setup.py --command-packages stdeb.command sdist_dsc --with-python2=true --with-python3=false bdist_deb
+    cd ../..
+else
+    echo "skipping Python 2 test"
+fi
 
-cd test_data/py3_only_pkg
-${PY3EXE} setup.py --command-packages stdeb.command sdist_dsc --with-python3=true --with-python2=false bdist_deb
-cd ../..
-
+if [ "$DO_PY3" = true ]; then
+    echo "using Python 3 at ${PY3EXE}"
+    cd test_data/py3_only_pkg
+    ${PY3EXE} setup.py --command-packages stdeb.command sdist_dsc --with-python3=true --with-python2=false bdist_deb
+    cd ../..
+else
+    echo "skipping Python 3 test"
+fi
