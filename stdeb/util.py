6740c8149bc21e64ea0416bc893b56a7c33a9d5e
Steven! Ragnarök <steven@nuclearsandwich.com>
2021-06-25 15:10:35 -0700

    Add dh_python3_params to override_dh_python3.

--- stdeb/util.py
+++ stdeb/util.py
@@ -1012,6 +1012,8 @@ class DebianInfo:
             self.dh_binary_arch_lines = '\tdh binary-arch'
         self.dh_binary_indep_lines = '\tdh binary-indep'
 
+        dh_python3_params = parse_val(cfg, module_name, 'dh-python3-params')
+
         conflicts = parse_vals(cfg, module_name, 'Conflicts')
         conflicts3 = parse_vals(cfg, module_name, 'Conflicts3')
         breaks = parse_vals(cfg, module_name, 'Breaks')
@@ -1199,6 +1201,7 @@ class DebianInfo:
             'scripts': scripts
         }
 
+        scripts = ''
         if force_x_python3_version and with_python3 and x_python3_version and \
                 x_python3_version[0]:
             # override dh_python3 target to modify the dependencies
@@ -1206,11 +1209,14 @@ class DebianInfo:
             version = x_python3_version[0]
             if not version.endswith('~'):
                 version += '~'
-            self.override_dh_python3 = RULES_OVERRIDE_PYTHON3 % {
-                'scripts': (
+                scripts = (
                     '        sed -i ' +
                     r'"s/\([ =]python3:any (\)>= [^)]*\()\)/\\1%s\\2/g" ' +
                     'debian/%s.substvars') % (version, self.package3)
+        if len(scripts) or len(dh_python3_params):
+            self.override_dh_python3 = RULES_OVERRIDE_PYTHON3 % {
+                'scripts': scripts,
+                'dh_python3_params': dh_python3_params,
             }
         else:
             self.override_dh_python3 = ''
@@ -1674,7 +1680,7 @@ override_dh_python2:
 """
 RULES_OVERRIDE_PYTHON3 = """
 override_dh_python3:
-        dh_python3
+        dh_python3 %(dh_python3_params)s
 %(scripts)s
 """
 
