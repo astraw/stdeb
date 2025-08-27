430f6cb9ad7e7d8e6d8c18450269f4401b20b879
Steven! Ragnarök <steven@nuclearsandwich.com>
2025-08-27 16:14:29 -0700

    Make setuptools a hard dependency.
    As a result of this change we also need to bootstrap the installation of
    stdeb as deb since we can't use its setup.py commands from the local
    directory.

--- setup.py
+++ setup.py
@@ -15,6 +15,7 @@ setup(
     long_description=long_description,
     license='MIT',
     url='http://github.com/astraw/stdeb',
+    install_requires=['setuptools'],
     packages=['stdeb', 'stdeb.command'],
     entry_points={'distutils.commands': ["%(cmd)s = stdeb.command.%(cmd)s:%(cmd)s" % {'cmd': x}
                                          for x in ('bdist_deb', 'debianize', 'install_deb', 'sdist_dsc', )], },
